b0a105918a0e9f8922bd696a5ca9476cc12ce7df
Utsav Subedi <subediutsav8@gmail.com>
2023-08-12 18:06:15 +0000

    Finish custom filters

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 def index(request):
+    all_posts = Post.objects.all()
+    print('all posts: ',all_posts)
     return render(request, "blog/index.html")
